682f60001f5d13fce73811c2effaf9b4e826aff8
hellhound94 <abnercordeiro@outlook.com>
2020-10-11 18:10:40 -0300

    Update M74HC164_lib.c

--- M74HC164_lib.c
+++ M74HC164_lib.c
@@ -1,18 +1,14 @@
 
-/* Definindo a função de cada pino */
+/* Definindo a funÃ§Ã£o de cada pino */
 sbit SERIAL_A at RC5_bit;
-sbit SERIAL_B at RC4_bit;
-
-sbit CLEAN at RC3_bit;
 sbit CLOCK at RC6_bit;
 
-/* Basta chamar essa função com o valor em ucByte */
+/* Basta chamar essa funÃ§Ã£o com o valor em ucByte */
 void M74HC164(unsigned char ucByte)
 {
     int x = 0;
     char flag = 0x01;
     
-    CLEAN = 1;
     CLOCK = 0;
     
     for(x = 0;x < 8;x++)
