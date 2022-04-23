1448e3de5e6357cf0af3a9141b83d71c8d083239
Abner Cordeiro de Paula <abnercordeiro@outlook.com>
2022-04-23 16:35:48 -0300

    Update M74HC164_lib.c
    Correção | estava com os pinos invertidos
    E corrigi de char para unsigned char

--- M74HC164_lib.c
+++ M74HC164_lib.c
@@ -1,23 +1,24 @@
 
 /* Definindo a função de cada pino */
-sbit SERIAL_A at RC5_bit;
+sbit SERIAL_B at RC5_bit;
 sbit CLOCK at RC6_bit;
 
 /* Basta chamar essa função com o valor em ucByte */
 void M74HC164(unsigned char ucByte)
 {
     int x = 0;
-    char flag = 0x01;
+    unsigned char flag = 0x01;
     
     CLOCK    = 0;
-    SERIAL_A = 0;
+    SERIAL_B = 0;
     for(x = 0;x < 8;x++)
     {
-         if( (ucByte & flag) == flag ) SERIAL_A = 1;
-         else SERIAL_A = 0;
+         if( (ucByte & flag) == flag ) SERIAL_B = 1;
+         else SERIAL_B = 0;
          flag <<= 1;
          CLOCK = 1;
          delay_us(1);
          CLOCK = 0;
     }
+    SERIAL_B = 0;
 }
